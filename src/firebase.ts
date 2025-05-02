68ccc0caca0a08d7cf600c1b346182a97ac3114e
santiago <santiportugal15@gmail.com>
2025-05-02 17:35:46 -0500

    Fix credentials

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSmScxHbzQiipkSCnaF_USs7BGu3C_9XY",
-  authDomain: "holaapp-66ec5.firebaseapp.com",
-  projectId: "holaapp-66ec5",
-  storageBucket: "holaapp-66ec5.firebasestorage.app",
-  messagingSenderId: "362038882821",
-  appId: "1:362038882821:web:1b29242978f91627b0226c"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
